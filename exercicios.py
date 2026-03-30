bc2df8286bcf404937ca1b15dfb5db70e239811d
Gabriel <gabriel.a.c.crispim2008@gmail.com>
2026-03-30 18:27:05 -0300

    Add files via upload

---
+++ exercicios.py
@@ -0,0 +1,228 @@
+# 1)
+
+# a = float(input("Digite um número: "))
+
+# if a >= 0:
+#   print("Número positivo")
+# else:
+#   print("número negativo")
+
+#-------------------------------------------------------#
+
+# 2)
+# n1_in = int(input("Digite um número: "))
+# print(n1_in * 2)
+
+#-------------------------------------------------------#
+
+# 3)
+
+# n1 = (input("Digite um número: "))
+
+# print(f"Oque vc escreveu: {n1}, e a classe dele é {type(n1)}")
+
+#-------------------------------------------------------#
+
+# 4)
+
+# n1 = int(input("Digite um número"))
+
+# if n1 %2 == 0:
+#   print("o número é par")
+# else:
+#   print("O número é impar")
+
+#-------------------------------------------------------#
+
+# 5)
+
+# n1 = input("Digite um número: ")
+
+# n1_int = int(n1)
+# triplo = n1_int * 3
+# print(triplo)
+
+#-------------------------------------------------------#
+
+# 6)
+
+# n1 = float(input("Digite um número: "))
+# n2 = float(input("Digite um número: "))
+
+# if n1 > n2:
+#   print(f"O número {n1} é maior que {n2}")
+# else:
+#   print(f"o número {n1} é menor que {n2}")
+
+#-------------------------------------------------------#
+
+# 7)
+
+# n1 = float(input("Digite um número: "))
+
+# if n1 > 10:
+#   print(f"o número {n1} é maior que 10")
+# else:
+#   print(f"o número {n1} é menor ou igual a 10")
+
+# 8)
+
+# n1 = float(input("Digite um número: "))
+
+# if n1 > 0:
+#   print( (n1 ** 1/2 ))
+# else:
+#   print("Número invalido")
+
+#-------------------------------------------------------#
+
+# 9)
+
+# n1 = (input("Digite um número: "))
+# n1_fl = float(n1)
+
+# print(n1_fl / 2)
+
+#-------------------------------------------------------#
+
+# 10)
+
+# n1 = float(input("Digite um número: "))
+
+# if n1 in range(10):
+#   print("Número está no intervalo")
+# else:
+#   print("Número NÃO está no intervalo")
+
+# 11)
+
+# n1 = float(input("Digite um número: "))
+
+# if n1 %2 == 0 and n1 > 0:
+#   print("Par positivo")
+# elif n1 %2 == 0 and n1 < 0:
+#   print("Par negativo")
+# else:
+#   print("Impar")
+
+#-------------------------------------------------------#
+
+# 12)
+
+# n1 = float(input("Digite um número: "))
+# n2 = float(input("Digite um número: "))
+
+# soma = n1 + n2
+
+# maior = n1
+
+# if maior < n2:
+#   maior = n2
+# elif n1 == n2:
+#   print(f"Os números são iguais")
+
+
+# print(f"A soma dos númeors é {soma} o maior número é {maior}")
+
+#-------------------------------------------------------#
+
+# 13)
+
+# n1 = float(input("Digite um número: "))
+
+# if n1 > 100:
+#   print(n1 / 2)
+# else:
+#   print(n1 * 2)
+
+#-------------------------------------------------------#
+
+# 14)
+
+# n1 = int(input("Digite um número: "))
+
+# if n1 %3 == 0:
+#   print(f"O número {n1} é múltiplo de 3")
+# else:
+#   print(f"{n1} Não é múltiplo")
+
+#-------------------------------------------------------#
+
+# 15)
+
+# n1 = int(input("Digite um número: "))
+
+# if n1 in range(10, 20):
+#   print("Dentro")
+# else:
+#   print("Fora")
+
+#-------------------------------------------------------#
+
+# 16)
+
+# n1 = input("Digite algo: ")
+# print("Tipo original ", type(n1))
+
+# try:
+#   numero = float(n1)
+#   dobro = numero ** 2
+#   print(f"É numérico {numero} e o Dobro é {dobro}")
+
+# except ValueError:
+#   print("Não é numérico")
+
+#-------------------------------------------------------#
+
+# 17)
+
+# n1 = int(input("Digite um número: "))
+
+# if n1 < 18:
+#   print("É menor de idade")
+# elif n1 < 59:
+#   print("É Adulto")
+# else:
+#   print("É idoso")
+
+#-------------------------------------------------------#
+
+# 18)
+
+# n1 = float(input("Digite um número: "))
+
+# if n1 %2 == 0 and n1 > 0:
+#   print("Par positivo")
+# elif n1 %2 == 0 and n1 < 0:
+#   print("Par negativo")
+
+# if n1 %2 == 1 and n1 > 0:
+#   print("Impar positivo")
+# elif n1 %2 == 1 and n1 < 0:
+#   print("impar negativo")
+
+#-------------------------------------------------------#
+
+# 19)
+
+# n1 = float(input("Digite um número: "))
+# n2 = float(input("Digite um número: "))
+
+# dif = (n1 - n2)
+
+# if n1 != n2:
+#   print(f"Eles são diferentes e a diferença é {dif}")
+# else:
+#   print("São iguais")
+
+#-------------------------------------------------------#
+
+# 20)
+
+# n1 = float(input("Digite um número: "))
+
+# if n1 in range(100):
+#   print("Está dentro do intervalo")
+# else:
+#   print(f"{n1} está fora do intervalo")
+
